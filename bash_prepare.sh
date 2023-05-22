6466d402c20bd79a02368f2af2d2f33393468d13
Yujian-Fu <yujian_fu@outlook.com>
2023-05-23 02:38:28 +0800

    test

--- bash_prepare.sh
+++ bash_prepare.sh
@@ -8,13 +8,13 @@ echo ${base_vecs_path}
 echo ${base_bin_path}
 echo ${query_vecs_path}
 echo ${query_bin_path}
-#./build/tests/utils/fvecs_to_bin ${data_type} ${base_vecs_path} ${base_bin_path}
-#./build/tests/utils/fvecs_to_bin ${data_type} ${query_vecs_path} ${query_bin_path}
+./build/tests/utils/fvecs_to_bin ${data_type} ${base_vecs_path} ${base_bin_path}
+./build/tests/utils/fvecs_to_bin ${data_type} ${query_vecs_path} ${query_bin_path}
 
 if [ $Target_task = "normal"  -o  $Target_task = "all" ]; then
 
 echo "##########################Compute the groundtruth of normal search##########################"
-#./build/tests/utils/compute_groundtruth  --data_type ${data_type} --dist_fn ${dist_fn} --base_file ${base_bin_path} --query_file  ${query_bin_path} --gt_file ${gt_path} --K ${K}
+./build/tests/utils/compute_groundtruth  --data_type ${data_type} --dist_fn ${dist_fn} --base_file ${base_bin_path} --query_file  ${query_bin_path} --gt_file ${gt_path} --K ${K}
 
 fi
 
